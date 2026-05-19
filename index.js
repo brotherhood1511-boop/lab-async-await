f2840f2c7ae2c9a88acc69c00007dde62588b055
Dominick Siciliano <brotherhood1511@gmail.com>
2026-05-19 00:44:52 -0400

    lab finished

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+const postList = document.getElementById("post-list");
+
+function displayPosts(posts) {
+posts.forEach((post) => {
+const li = document.createElement("li");
+
+const h1 = document.createElement("h1");
+h1.textContent = post.title;
+
+const p = document.createElement("p");
+p.textContent = post.body;
+
+li.append(h1, p);
+postList.append(li);
+});
+}
+
+async function fetchPosts() {
+const response = await fetch(
+"https://jsonplaceholder.typicode.com/posts"
+);
+
+const posts = await response.json();
+
+displayPosts(posts);
+}
+
+fetchPosts();
